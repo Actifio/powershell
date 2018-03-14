81576d076a11b938dda702424d1eac74d40a2406
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2018-03-14 13:55:23 +1100

    Update mail.ps1

--- emailreporter/mail.ps1
+++ emailreporter/mail.ps1
@@ -1,23 +1,26 @@
-####   This script should not require any changes
+####   This script should not require any changes unless you want to change email formatting
 param([string]$configfile = "configfile")
 . ./$configfile
+$currentdate = (get-date (get-date) -UFormat "%Y-%m-%d %H:%M:%S")
 $data = Import-Csv -Path $appliancelist -Header "ApplianceName","ApplianceIP" -Delimiter ","
-$mailbody = "<html>"
-$mailbody += "<body>"
-$mailbody += "<pre style=font: monospace>"
+$mailbody = "<html> `n"
+$mailbody += "<body> `n"
+$mailbody += "<pre style=font: monospace> `n"
+$mailbody += "Report created on $currentdate `n"
+$mailbody += "############################################################################# `n"
 $addresses = Get-content "iplist.txt"
 foreach ($item in $data){
   if (!$keyfile) { Connect-Act -acthost $item.ApplianceIP -actuser $user -password $password -ignorecerts }
   if (!$password) { Connect-Act -acthost $item.ApplianceIP -actuser $user -passwordfile $keyfile -ignorecerts }
-  $mailbody += "##################     "
-  $mailbody += $item.ApplianceName
-  $mailbody += "     ##################"
+  $mailbody += "Appliance:  "
+  $mailbody += $item.ApplianceName 
   $mailbody += "`n"
   $mailbody += Invoke-Expression $command | format-table | out-string
+  $mailbody += "---------------------------------- `n"
   disconnect-act
 }
-$mailbody += "</pre>"
-$mailbody += "</body>"
-$mailbody += "</html>"
-$mailbody
+$mailbody += "</pre> `n"
+$mailbody += "</body> `n"
+$mailbody += "</html> `n"
 Send-MailMessage -From $fromaddr -To $dest -Body $mailbody -SMTP $mailserver -subject $subject -BodyAsHtml 
+
