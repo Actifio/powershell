cc87947f2ddb922dcca32c4a109dce2a2d421d01
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2018-03-15 14:38:15 +1100

    Update config.ps1

--- emailreporter/config.ps1
+++ emailreporter/config.ps1
@@ -9,11 +9,15 @@
 # The file needs to contain one line for each appliance in the format:    appliancename,applianceip
 [string] $appliancelist = "./appliancelist.txt"
 
-#  this variable is the command we will run on the Actifio Appliance
+#  this variable is the command we will run on the Actifio Appliance.  The command needs to be in double quotes
+#  You may need to use different quoting if double quotes are needed inside the select, such as:   
+#  $command = 'reportapps -gc | select apptype,appname,"mdlstat(gb)","total(gb)","postcompress(gb)","organizations"'
 [string] $command = "reportsnaps -d1 | select startdate,hostname,appname,capturetype"
 
 # Destination addresses.  Make sure to add extra addrs with the same format
+# Add as many addresses as you like, or delete all but one
 $dest = "anthonyv@acme.com","aarontully@acme.com"
+
 # from address for the email
 [string] $fromaddr = "actifioreporter@acme.com"
 # subject line for the email
